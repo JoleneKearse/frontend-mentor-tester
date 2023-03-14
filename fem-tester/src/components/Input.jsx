b44e230a6df31ff8ddbd9cb4ab0317a2b2c7e8de
Jolene Kearse <pharaohnutz@gmail.com>
2023-03-14 12:13:39 -0400

    refactor: clean up css & adjust margins

--- fem-tester/src/components/Input.jsx
+++ fem-tester/src/components/Input.jsx
@@ -5,9 +5,12 @@ const Input = () => {
     <div id="inputBlock">
       <h2>Params</h2>
       <p>Simply enter your <span className='gradient-text bold'>deployed site</span> and check out the results!</p>
-      <label htmlFor="website">
-        <input type="url" name="website" id="websiteUrl" placeholder='https://frontend-mentor-project.vercel.app'/>
-      </label>
+      <form action="" id="siteInput" className='flex'>
+        <label htmlFor="website">
+          <input type="url" name="website" id="websiteUrl" placeholder='https://frontend-mentor-project.vercel.app'/>
+        </label>
+        <button type="submit">Go!</button>
+      </form>
     </div>
   )
 }
