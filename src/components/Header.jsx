020f34a3e376063221e3c974a717f4b905e1a0d6
Jolene Kearse <pharaohnutz@gmail.com>
2023-04-09 15:24:33 -0400

    feat: update intro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,12 @@ const Header = () => {
   return (
     <div className='header'>
       <h1 className='title gradient-text text-stroke'>Frontend Mentor <span class="title-deco">Tester</span></h1>
-      <p className="intro"><a href="https://www.frontendmentor.io/" target="_blank" rel="noopener noreferrer">Frontend Mentor</a> is an <span className='gradient-text bold'>awesome</span> way to:</p>
-      <ul className='intro-list'>
-        <li><span className='gradient-text bold'>challenge</span> yourself</li>
-        <li><span className='gradient-text bold'>test</span> your skills</li>
-        <li>& just have <span className='gradient-text bold'>fun</span></li>
 
-      </ul>
-      <p className="intro">But finding <span className='gradient-text bold'>quick</span> & <span className='gradient-text bold'>simple</span> ways to test the <span className='gradient-text bold'>375px mobile</span> and <span className='gradient-text bold'>1440px desktop</span> views can be a pain.</p>
-      <p className="intro">This is my solution! Enter your params below, hit '<span className='gradient-text bold'>Go!</span>', then in just a few seconds, you'll be able to compare your coded result.</p>
+      <p className="intro">Tired of spending all that <span className='gradient-text bold'>exhausting</span> time testing out <span className='gradient-text bold'>375px mobile</span> and <span className='gradient-text bold'>1440px desktop</span> views of your <a href="https://www.frontendmentor.io/" target="_blank" rel="noopener noreferrer">Frontend Mentor</a> <span className='gradient-text bold'>challenges?</span>.</p>
+
+      <p className="intro">Okay, maybe it's not <span className='gradient-text bold'>sooo</span> much time, but let's <span className='gradient-text bold'>speed it up</span> anyway!</p>
+
+      <p className="intro">Enter your params below, hit '<span className='gradient-text bold'>Go!</span>', then compare your coded result in <span className='gradient-text bold'>seconds</span>.</p>
     </div>
   )
 }
