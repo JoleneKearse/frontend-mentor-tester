c2a383224b39d0f1710036cd6cf68956bb0b7d45
Jolene Kearse <pharaohnutz@gmail.com>
2023-04-09 16:08:59 -0400

    style: add midjourney image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,18 @@ const Header = () => {
     <div className='header'>
       <h1 className='title gradient-text text-stroke'>Frontend Mentor <span class="title-deco">Tester</span></h1>
 
-      <p className="intro">Tired of spending all that <span className='gradient-text bold'>exhausting</span> time testing out <span className='gradient-text bold'>375px mobile</span> and <span className='gradient-text bold'>1440px desktop</span> views of your <a href="https://www.frontendmentor.io/" target="_blank" rel="noopener noreferrer">Frontend Mentor</a> <span className='gradient-text bold'>challenges?</span>.</p>
+      <div className="side-by-side">
+        <div>
+          <img src="src/assets/cover-img.png" alt="anime girl stressed out testing mobile and desktop sites" className='cover-img' />
+        </div>
+        <div className="intro-text">
+          <p className="intro">Tired of spending all that <span className='gradient-text bold'>exhausting</span> time testing out <span className='gradient-text bold'>375px mobile</span> and <span className='gradient-text bold'>1440px desktop</span> views of your <a href="https://www.frontendmentor.io/" target="_blank" rel="noopener noreferrer">Frontend Mentor</a> <span className='gradient-text bold'>challenges?</span>.</p>
+          <p className="intro">Okay, maybe it's not <span className='gradient-text bold'>sooo</span> much time, but let's <span className='gradient-text bold'>speed it up</span> anyway!</p>
+          <p className="intro">Enter your params below, hit '<span className='gradient-text bold'>Go!</span>', then compare your coded result in <span className='gradient-text bold'>seconds</span>.</p>
+        </div>
+      </div>
 
-      <p className="intro">Okay, maybe it's not <span className='gradient-text bold'>sooo</span> much time, but let's <span className='gradient-text bold'>speed it up</span> anyway!</p>
 
-      <p className="intro">Enter your params below, hit '<span className='gradient-text bold'>Go!</span>', then compare your coded result in <span className='gradient-text bold'>seconds</span>.</p>
     </div>
   )
 }
